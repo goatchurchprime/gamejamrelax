f88d3314cf664f2bc3d9fdc6d73dd207edddfb54
Julian Todd <julian@goatchurch.org.uk>
2024-07-08 15:08:26 +0100

    the stillness score from the head tracking

---
+++ main.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+var stillnessscore = 0.0
+var prevheadtransform : Transform3D = Transform3D()
+const eyenosevector = Vector3(0, -0.2, -0.2)
+
+func _process(delta):
+	var headtransform = $XROrigin3D/XRCamera3D.global_transform
+	var vecoriginchange = prevheadtransform.origin - headtransform.origin
+	var vecnosechange = prevheadtransform*eyenosevector - headtransform*eyenosevector
+	var dvecoriginchange = vecoriginchange.length()/delta
+	var dvecnosechange = vecnosechange.length()/delta
+	#print(dvecnosechange, " ", dvecoriginchange)
+	prevheadtransform = headtransform
+	if dvecnosechange < 0.015 and dvecoriginchange < 0.015:
+		stillnessscore += delta
+	else:
+		stillnessscore = stillnessscore*0.9
+	$XROrigin3D/StillnessLabel3D/StillnessScore.text = "%.1f" % stillnessscore
