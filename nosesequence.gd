a36e3f5b919968bed85b00e9ab7e7f9948f1e503
Julian Todd <julian@goatchurch.org.uk>
2024-07-09 16:25:39 +0100

    add some nose sequence stuff

---
+++ nosesequence.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+@onready var headcontroller = XRHelpers.get_xr_camera(get_node("../XROrigin3D"))
+
+var nosespotI = 0
+func _ready():
+	var nosespot0 = $Spot0
+	for i in range(1, 40):
+		var nosespoti = nosespot0.duplicate()
+		nosespoti.name = "Spot%d" % i
+		add_child(nosespoti)
+		
+func _physics_process(delta):
+	get_child(nosespotI).transform = headcontroller.get_node("NosePointer").global_transform
+	nosespotI = (nosespotI + 1) % get_child_count()
